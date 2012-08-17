ef7ccefae952e1a917588c5ca8618315413b18cd
Giulio Cervera <giulio.cervera@gmail.com>
2012-08-17 11:25:39 +0200

    nozomi: cleanup libv8
    Change-Id: Ifeefd5450eba45ffe4a011cac041eb5d63b06e15

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -26,8 +26,7 @@ MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 PRODUCT_COPY_FILES := \\
     $OUTDIR/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so \\
     $OUTDIR/proprietary/lib/libacdbloader.so:obj/lib/libacdbloader.so \\
-    $OUTDIR/proprietary/lib/libacdbmapper.so:obj/lib/libacdbmapper.so \\
-    $OUTDIR/proprietary/lib/libv8.so:obj/lib/libv8.so
+    $OUTDIR/proprietary/lib/libacdbmapper.so:obj/lib/libacdbmapper.so
 
 PRODUCT_COPY_FILES += \\
 EOF
