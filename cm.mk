b158c0aa73779e1caf24737a44c6fef2782d8445
Giulio Cervera <giulio.cervera@gmail.com>
2012-08-21 12:30:15 +0200

    nozomi: remove bootanimation settings
    moved to fuji-common as most (if not all) fuji have a 1280x720 panel
    
    Change-Id: I37cfdc5c8145f2471f7b628fe01774f72d4e4eaa

--- cm.mk
+++ cm.mk
@@ -11,7 +11,5 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i_1257-4009 BUILD_FINGERPRINT=SEMC/LT26i_1257-4009/LT26i:4.0.4/6.1.A.0.452/O_5_zw:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.0.4 6.1.A.0.452 O_5_zw test-keys"
 
-TARGET_BOOTANIMATION_NAME := vertical-720x1280
-
 PRODUCT_NAME := cm_nozomi
 PRODUCT_DEVICE := nozomi
