6642430ce5824e319c77e5b21b6f7d2411467c8c
Giulio Cervera <giulio.cervera@gmail.com>
2012-08-21 12:30:15 +0200

    nozomi: set width and height for cm10 bootanimation
    Change-Id: I37cfdc5c8145f2471f7b628fe01774f72d4e4eaa

--- cm.mk
+++ cm.mk
@@ -6,12 +6,14 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i_1257-4009 BUILD_FINGERPRINT=SEMC/LT26i_1257-4009/LT26i:4.0.4/6.1.A.0.452/O_5_zw:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.0.4 6.1.A.0.452 O_5_zw test-keys"
-
-TARGET_BOOTANIMATION_NAME := vertical-720x1280
 
 PRODUCT_NAME := cm_nozomi
 PRODUCT_DEVICE := nozomi
