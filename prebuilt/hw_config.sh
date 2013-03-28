7973015fc64ab8b4c310a7ee99b1507050bce038
Alin Jerpelea <jerpelea@gmail.com>
2013-03-28 19:57:18 +0200

    nozomi: delete obsolete als config
    Change-Id: I84e54488d11d2bdf6e906b7c25691fa45bd5f4f6

--- prebuilt/hw_config.sh
+++ prebuilt/hw_config.sh
@@ -37,9 +37,6 @@ echo $val_filter > $dev/filter  # RFilter. Valid range is 0 - 3.
 
 # LMU AS3676 Configuration
 dev=/sys/class/leds
-echo 1,51,255,39,20,6,84 > $dev/lcd-backlight/als/curve  # ALS curve for group1
-echo 1,0,0,0 > $dev/lcd-backlight/als/params  #[gain],[filter_up],[filter_down],[offset]
-echo 1 > $dev/lcd-backlight/als/enable  #Sensor on/off. 1 = on, reg 90h
 echo 2000 > $dev/button-backlight/max_current
 
 # TI BQ275xx firmware loader
