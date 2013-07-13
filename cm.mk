c4f34a6ded6cdbf71fc16186c61e2ce20b790e41
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-07-13 14:25:20 +0200

    nozomi: update for 4.1.2 blob and kernel
    Change-Id: I44ce0f09ed24f0e4597dd4eae8d0fc4dfeda65b9

--- cm.mk
+++ cm.mk
@@ -13,7 +13,7 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i_1257-5499 BUILD_FINGERPRINT=SEMC/LT26i_1257-5499/LT26i:4.0.4/6.1.A.2.50/zfd_zw:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.0.4 6.1.A.2.50 zfd_zw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i BUILD_FINGERPRINT=SEMC/LT26i_1257-6919/LT26i:4.1.2/6.2.B.0.200/N7__zg:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.1.2 6.2.B.0.200 N7__zg test-keys"
 
 PRODUCT_NAME := cm_nozomi
 PRODUCT_DEVICE := nozomi
