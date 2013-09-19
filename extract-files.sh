4aeaaea9053ba0779aa6edefb136642f3364b8e8
Arnav Gupta <championswimmer@aokp.co>
2013-09-19 09:45:29 +0530

    nozomi: move extract-files to common
    Change-Id: I07ddf311ef1c5c35a2b339b143f5bf13054918a0
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- extract-files.sh
+++ extract-files.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
 
-VENDOR=sony
-DEVICE=nozomi
+export DEVICE=${PWD##*/}
+export BOARDCONFIGVENDOR=true
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+../common/extract-files.sh $@
 
-while getopts ":nh" options
-do
-  case $options in
-    n ) NC=1 ;;
-    h ) echo "Usage: `basename $0` [OPTIONS] "
-        echo "  -n  No clenup"
-        echo "  -h  Show this help"
-        exit ;;
-    * ) ;;
-  esac
-done
+../common/setup-makefiles.sh
 
-if [ "x$NC" != "x1" ];
-then
-    rm -rf $BASE/*
-fi
-
-for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
-    # Split the file from the destination (format is "file[:destination]")
-    OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-    FILE=${PARSING_ARRAY[0]}
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    adb pull /system/$FILE $BASE/$FILE
-done
-
-./setup-makefiles.sh
