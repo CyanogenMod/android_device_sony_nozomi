4b54ed2ba085b672a8bb2f29a46fb083fc66fc9a
Arnav Gupta <championswimmer@aokp.co>
2013-09-19 21:41:48 +0530

    nozomi: update fingerprint
    Change-Id: I3ea24b53ec2699cad129e4e41a16e25e7a5a6772
    Signed-off-by: Arnav Gupta <championswimmer@aokp.co>

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i_1257-5499 BUILD_FINGERPRINT=SEMC/LT26i_1257-5499/LT26i:4.0.4/6.1.A.2.50/zfd_zw:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.0.4 6.1.A.2.50 zfd_zw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i BUILD_FINGERPRINT=SEMC/LT26i_1257-6919/LT26i:4.1.2/6.2.B.0.200/N7__zg:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.1.2 6.2.B.0.200 N7__zg test-keys"
 
 PRODUCT_NAME := cm_nozomi
 PRODUCT_DEVICE := nozomi
